ebe1621d8f355ae0159b235639289cd6aa72464b
brunohro <brunoh.rodrigues27@gmail.com>
2025-05-24 15:14:16 -0300

    Criando as models para livros

--- minha_estante/admin.py
+++ minha_estante/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Autores, Categoria, Livro
 
-# Register your models here.
+@admin.register(Autores)
+class AutoresAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('nome', )
+
+@admin.register(Livro)
+class LivroAdmin(admin.ModelAdmin):
+    list_display = ('nome', )
--- minha_estante/models.py
+++ minha_estante/models.py
@@ -1,2 +1,23 @@
 from django.db import models
 
+class Categoria(models.Model):
+    nome = models.CharField(max_length=200)
+    def __str__(self):
+        return self.nome
+
+class Autores(models.Model):
+    nome = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nome
+class Livro(models.Model):
+    nome = models.CharField(max_length=200)
+    descricao = models.TextField(max_length=200)
+    autor = models.ForeignKey(Autores, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    is_completed = models.BooleanField()
+    imagem = models.ImageField(upload_to='livros/', blank=True, null=True)
+    data_inicio = models.DateField()
+    data_fim = models.DateField()
+
+
